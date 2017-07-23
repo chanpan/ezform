a5ac8d69e5213877e4930739b0d39e9ff8f660a1
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2017-07-24 02:56:26 +0700

    master

--- controllers/main.js
+++ controllers/main.js
@@ -1,12 +1,18 @@
-
-module.exports = routing=(url="", selector="")=>{
-    $.ajax({
-        url:url,
-        success:function(data){
-            $(`#${selector}`).html(data);
-        }
-    });
+ 
+var c = module.exports = {
+    content:(url="", selector="")=>{
+        return c.routings(url,selector);
+    },
+    routings:(url="", selector="")=>{
+        $.ajax({
+                url:url,
+                success:function(data){
+                    $(`#${selector}`).html(data);
+                }
+        });
+    }
 }
  //routing("./views/layouts/navbar.html", "navbar");
- routing("./views/ezform/index.html", "root");
+ 
+
  
